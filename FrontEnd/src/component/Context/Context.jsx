73921cde9fa52a663f7db292272fbddc87efe9a5
Khalid Bouychou <khbouych@e3r7p17.1337.ma>
2024-09-23 12:27:14 +0100

    update

--- FrontEnd/src/component/Context/Context.jsx
+++ FrontEnd/src/component/Context/Context.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
-import { createContext ,useEffect,useState } from "react";
-import {  useLocation, useNavigate } from "react-router-dom";// Add missing import statement
+import { createContext, useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom"; // Add missing import statement
 // import { useNavigate } from "react-router-dom";
 
 export const authContext = createContext();
@@ -10,69 +10,66 @@ export default function ContextProvider({ children }) {
   const navigate = useNavigate();
   const location = useLocation();
 
-
-    async function auth() {
-      try {
-        const urlParams =  new URLSearchParams(location.search);
-        const error =  urlParams.get("error");
-        if (error) {
+  async function auth() {
+    try {
+      const urlParams = new URLSearchParams(location.search);
+      const error = urlParams.get("error");
+      if (error) {
+        navigate("/login");
+      }
+      const code = urlParams.get("code");
+      if (code) {
+        const params = new URLSearchParams();
+        params.append("code", code);
+        const res = await axios.post(
+          `http://localhost:8000/api/login/`,
+          params,
+          {
+            withCredentials: true,
+          }
+        );
+        if (res.data.status === 200) {
+          navigate("/home");
+          setUser(res.data.user);
+        } else {
           navigate("/login");
         }
-        const code =  urlParams.get("code");
-        if (code) {
-          const params = new URLSearchParams();
-          params.append("code", code);
-          const res = await axios.post(
-            `http://localhost:8000/api/login/`,
-            params,
-            {
-              withCredentials: true,
-            }
-          );
-          if (res.data.status === 200 ) 
-          {
-            navigate("/home");
-            setUser(res.data.user);
-          }
-          else
-          {
-            navigate("/login");
-          }
-        }
-      } catch (error) {
-        console.log(error);
       }
+    } catch (error) {
+      console.log(error);
     }
-
-    useEffect(() => {
-      auth();
-    }, []);
-
-  async function verifytoken() {
-   await axios.post("http://localhost:8000/api/verifytoken/", {
-    user : user
-    }, {
-      withCredentials: true,
-      headers: {
-        "Content-Type": "application / json", 
-   }
-    }).then((res) => {
-      if (res.data.status === 200) {
-        setUser(res.data.user);
-      } else {
-       return navigate("/login");
-      }
-    }
-    ).catch((err) => {
-      console.log(err);
-      return navigate("/login");
-    });
-  
   }
 
+  useEffect(() => {
+    auth();
+  }, []);
 
-
-
+  async function verifytoken() {
+    await axios
+      .post(
+        "http://localhost:8000/api/verifytoken/",
+        {
+          user: user,
+        },
+        {
+          withCredentials: true,
+          headers: {
+            "Content-Type": "application / json",
+          },
+        }
+      )
+      .then((res) => {
+        if (res.data.status === 200) {
+          setUser(res.data.user);
+        } else {
+          return navigate("/login");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        return navigate("/login");
+      });
+  }
 
   return (
     <authContext.Provider
--- backend/login/views.py
+++ backend/login/views.py
@@ -105,7 +105,8 @@ class PlayerViewSet(viewsets.ModelViewSet):
                 'Authorization': 'Bearer ' + token,
                 'Content-Type': 'application/json'
             }
-            response = requests.get('https://api.intra.42.fr/v2/me', headers=headers)
+            response = requests.get(
+                'https://api.intra.42.fr/v2/me', headers=headers)
             response.raise_for_status()  # Check if the request was successful
             intra_data = response.json()
 
@@ -115,11 +116,12 @@ class PlayerViewSet(viewsets.ModelViewSet):
                 'email': intra_data.get('email'),
             }
             # Check if user exists
-            user = Player.objects.filter(username=user_data['username']).first()
-            if not user :
+            user = Player.objects.filter(
+                username=user_data['username']).first()
+            if not user:
                 user = self.create_user(user_data)
 
-            print('user ---- >',user)
+            print('user ---- >', user)
             # Create JWT tokens
             tokens = self.create_jwt_token(user)
             # Create response
@@ -143,7 +145,7 @@ class PlayerViewSet(viewsets.ModelViewSet):
                 secure=True,
                 samesite='Lax'
             )
-            return  response
+            return response
         except requests.RequestException as e:
             return Response({'error': 'Request failed: {}'.format(str(e))}, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
@@ -153,14 +155,14 @@ class PlayerViewSet(viewsets.ModelViewSet):
         try:
             token = request.COOKIES.get('access')
             if not token:
-                return Response({'msg':'none'}, status=status.HTTP_200_OK)
+                return Response({'msg': 'none'}, status=status.HTTP_200_OK)
             response = Response({'msg': 'Token deleted'},
                                 status=status.HTTP_200_OK)
             response.delete_cookie('access')
             return response
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
-    
+
     def user_status(self, request):
         token = request.COOKIES.get('access')
         if not token:
@@ -190,25 +192,23 @@ class PlayerViewSet(viewsets.ModelViewSet):
             for player in serializer.data
         ]
         return Response(users)
-    def verifytoken (self , request) :
-        try :
+
+    def verifytoken(self, request):
+        try:
             token = request.COOKIES.get('token')
-            if not token :
-                return Response({'msg':'Token is invalid or expired'},status=status.HTTP_400_BAD_REQUEST)
+            if not token:
+                return Response({'msg': 'Token is invalid or expired'}, status=status.HTTP_400_BAD_REQUEST)
             decoded_token = AccessToken(token)
             payload = decoded_token.payload
             if not payload.get("user_id"):
-                return Response({'msg':'Token is invalid or expired '},status=status.HTTP_400_BAD_REQUEST)
+                return Response({'msg': 'Token is invalid or expired '}, status=status.HTTP_400_BAD_REQUEST)
             user = Player.objects.get(id=payload.get("user_id"))
-            print ('user ---- >', user)
+            print('user ---- >', user)
             data = {
-                'msg':'valid',
-                'user' : PlayerSerializer(user).data
+                'msg': 'valid',
+                'user': PlayerSerializer(user).data
             }
             return Response(data)
-        except Exception as e :
+        except Exception as e:
             print('error ---- >', e)
-            return Response({'msg':'Token is invalid or expired'},status=status.HTTP_400_BAD_REQUEST)
-
-
-
+            return Response({'msg': 'Token is invalid or expired'}, status=status.HTTP_400_BAD_REQUEST)
