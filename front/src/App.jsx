823c61468768c0e066c6e2e91998211dc5e6c39e
Khalid Bouychou <khbouych@e3r10p18.1337.ma>
2024-10-15 16:39:06 +0100

    front signin&signup

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,39 +1,41 @@
 // import React, { useContext, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Sidebar from './components/SideBar/Sidebar.jsx';
-import Games from './components/Game/Game.jsx';
+// import Sidebar from './components/SideBar/Sidebar.jsx';
+// import Games from './components/Game/Game.jsx';
 
-import PingPongGames from './components/Game/components/PingGame/PingGame.jsx';
-import XOGames from './components/Game/components/TicTac/TicTac.jsx';
+// import PingPongGames from './components/Game/components/PingGame/PingGame.jsx';
+// import XOGames from './components/Game/components/TicTac/TicTac.jsx';
 
-import Home from './components/Home/Home.jsx';
-import Chat from './Chat/Chat.jsx';
-import Profile from './components/Profile/Profile.jsx';
-import Setting from './components/Setting/Setting.jsx';
-import Notificationz from './components/Notification/Notification.jsx';
-import None from './components/None/None.jsx';
-import LocalGame from './ponggame/localpong/LocalGame.jsx';
-import LocalTeamGame from './ponggame/teampong/LocalTeamGame.jsx';
-import OnlineGame from './ponggame/onlinepong/OnlineGame.jsx';
-import Tournament from './ponggame/Tournamentpong/Tournament.jsx';
+// import Home from './components/Home/Home.jsx';
+// import Chat from './Chat/Chat.jsx';
+// import Profile from './components/Profile/Profile.jsx';
+// import Setting from './components/Setting/Setting.jsx';
+// import Notificationz from './components/Notification/Notification.jsx';
+// import None from './components/None/None.jsx';
+// import LocalGame from './ponggame/localpong/LocalGame.jsx';
+// import LocalTeamGame from './ponggame/teampong/LocalTeamGame.jsx';
+// import OnlineGame from './ponggame/onlinepong/OnlineGame.jsx';
+// import Tournament from './ponggame/Tournamentpong/Tournament.jsx';
 // import AuthProvider from './UserContext/Context.jsx';
 import style from './App.module.css';
-import Login from './Login/Login.jsx';
+// import Login from './Login/Login.jsx';
 // import { AuthContext } from './UserContext/Context.jsx';
 // import ProtectedRoutes from './protectedRoutes.jsx';
 
-import { NotificationWebSocketProvider } from './contexts/NotifWSContext.jsx';
-import Layout from './Layout.jsx';
+// import { NotificationWebSocketProvider } from './contexts/NotifWSContext.jsx';
+// import Layout from './Layout.jsx';
+import LoginSignup from './Login/SignupSignin/SignupSignin.jsx';
 
 function App() {
 
   return (
-    // <BrowserRouter>
+    <>
+    {/* // <BrowserRouter> */}
 
       <div className={style.EntirePage}>
         <div className={style.MainContent}>
           <Routes>
-          <Route path="/" element={<Layout />}>
+          {/* <Route path="/" element={<Layout />}> */}
               {/* <Route path="home" element={<NotificationWebSocketProvider><Home /></NotificationWebSocketProvider>} />
               <Route path="games" element={<NotificationWebSocketProvider><Games /></NotificationWebSocketProvider>} />
               <Route path="pingpong-games" element={<NotificationWebSocketProvider><PingPongGames /></NotificationWebSocketProvider>} />
@@ -45,8 +47,8 @@ function App() {
               <Route path="chat" element={<NotificationWebSocketProvider><Chat /></NotificationWebSocketProvider>} />
               <Route path="profile" element={<NotificationWebSocketProvider><Profile /></NotificationWebSocketProvider>} />
               <Route path="setting" element={<NotificationWebSocketProvider><Setting /></NotificationWebSocketProvider>} />
-              <Route path="notification" element={<NotificationWebSocketProvider><Notificationz /></NotificationWebSocketProvider>} /> */}
-              <Route path="home" element={<Home />} />
+            <Route path="notification" element={<NotificationWebSocketProvider><Notificationz /></NotificationWebSocketProvider>} /> */}
+              {/* <Route path="home" element={<Home />} />
               <Route path="games" element={<Games />} />
               <Route path="pingpong-games" element={<PingPongGames />} />
               <Route path="xo-games" element={<XOGames />} />
@@ -58,14 +60,16 @@ function App() {
               <Route path="profile" element={<Profile />} />
               <Route path="setting" element={<Setting />} />
               <Route path="notification" element={<Notificationz />} />
-            </Route>
-            <Route path="/*" element={<None />} />
+              </Route>
+              <Route path="/*" element={<None />} /> */}
             <Route path="/login" element={<Login />} />
+            {/* <Route path="/" element={<LoginSignup />} /> */}
           </Routes>
-        </div>
       </div>
-     // </BrowserRouter>
-  );
+      </div>
+      {/* // </BrowserRouter> */}
+      </>
+    );
 }
 
 export default App;
