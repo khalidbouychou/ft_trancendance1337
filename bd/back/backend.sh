79f87e1fce79b12a5952b054d3c915a8f2b16188
Khalid Bouychou <khbouych@e3r10p18.1337.ma>
2024-10-16 20:23:44 +0100

    reset

--- bd/back/backend.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-
-	python3 manage.py makemigrations
-	python3 manage.py migrate
-	python3 manage.py runserver 0.0.0.0:8000
-
-    daphne -e ssl:8000:privateKey=/path/to/private/key.pem:certKey=/path/to/certificate/cert.pem your_app.asgi:application
-
-
--- fd/src/components/khbouych/PrivateRoutes/Spinner.jsx
+++
@@ -1,8 +0,0 @@
-
-import './spinner.css'
-
-const Spinner = () => {
-  return (<div className="loading-spinner"></div>)
-}
-
-export default Spinner
