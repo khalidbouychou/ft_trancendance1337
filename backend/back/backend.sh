2924affec3a73c95eeb0a05adfdcf55628163449
Khalid Bouychou <khbouych@e2r2p15.1337.ma>
2024-10-17 23:16:36 +0100

    now u can run project on docker

--- backend/back/backend.sh
+++ backend/back/backend.sh
@@ -1,10 +1,13 @@
 #!bin/bash
 
 sleep 10
-python3 manage.py makemigrations
-python3 manage.py migrate
-python3 manage.py runserver 0.0.0.0:8000
+ 
+python manage.py makemigrations login chat matches notification pongame
+sleep 5
+python manage.py migrate
+sleep 5
+python manage.py runserver 0.0.0.0:8000
 
-daphne -e ssl:8000:privateKey=/path/to/private/key.pem:certKey=/path/to/certificate/cert.pem api.asgi:application
+# daphne -e ssl:8000:privateKey=/path/to/private/key.pem:certKey=/path/to/certificate/cert.pem api.asgi:application
 
 
