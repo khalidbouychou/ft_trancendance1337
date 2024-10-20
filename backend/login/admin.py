5a38990c0af93ad717e6e5944470c92b52820279
Khalid Bouychou <khbouych@e2r9p9.1337.ma>
2024-10-20 12:45:15 +0100

    reset

--- backend/login/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Player, Friend
-# Register your models here.
-
-
-admin.site.register(Player)
-admin.site.register(Friend)
-# admin.site.register(MatchHistory)
