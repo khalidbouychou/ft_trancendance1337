be32bd6a4de5d32772813bdfd53b0c91b0f44708
Khalid Bouychou <khalidbouychou22@gmail.com>
2024-10-20 17:32:47 +0100

    update in .env

--- front/src/UserContext/Context.jsx
+++ front/src/UserContext/Context.jsx
@@ -1,72 +1,75 @@
+import axios from 'axios'
+import { createContext, useEffect, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
 
-import axios from "axios";
-import { createContext, useEffect, useState } from "react";
-import { Navigate, useLocation, useNavigate } from "react-router-dom";
-import Cookies from 'js-cookie';
+export const AuthContext = createContext(null)
 
-export const AuthContext = createContext(null);
+export default function AuthProvider ({ children }) {
+  const [user, setUser] = useState(null)
+  const [url, setUrl] = useState('')
+  const [islogin, setIslogin] = useState(false)
+  const navigate = useNavigate()
+  const location = useLocation()
 
-export default function AuthProvider({ children }) {
-  const [user, setUser] = useState(null);
-  // const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [url, setUrl] = useState("");
-  const [islogin, setIslogin] = useState(false);
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  async function auth_intra42() {
-    const response = await axios.get("http://10.13.6.2:8000/api/auth_intra/");
+  async function auth_intra42 () {
+    const response = await axios.get('http://10.12.9.9:8000/api/auth_intra/', {
+      withCredentials: true
+    })
     try {
       if (response.status === 200) {
-        setUrl(response.data.url);
+        setUrl(response.data.url)
       }
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
   }
 
-  async function Login() {
+  async function Login () {
     try {
-      const urlParams = new URLSearchParams(location.search);
-      const error = urlParams.get("error");
-      if (error) {navigate("/login");}
-      const code = urlParams.get("code");
+      const urlParams = new URLSearchParams(location.search)
+      const error = urlParams.get('error')
+      if (error) {
+        navigate('/login')
+      }
+      const code = urlParams.get('code')
       if (code) {
-        const params = new URLSearchParams();
-        params.append("code", code);
-        const res = await axios.post(`http://10.13.6.2:8000/api/login/`,params,{
-          withCredentials: true
-        });
-        if (res.status === 200)
+        const params = new URLSearchParams()
+        params.append('code', code)
+        const res = await axios.post(
+          `http://10.12.9.9:8000/api/login/`,
+          params,
           {
-          console.log("res.data:", res.data);
-          const token = res.data.user.token;
-          console.log("token:", token);
-          localStorage.setItem('token', token);
-          setUser(res.data);
-          setIslogin(true);
-          console.log("i will send you to home");
-          navigate("/");
+            withCredentials: true
+          }
+        )
+        if (res.status === 200) {
+          console.log('res.data:', res.data)
+          const token = res.data.user.token
+          console.log('token:', token)
+          localStorage.setItem('token', token)
+          setUser(res.data)
+          setIslogin(true)
+          console.log('i will send you to home')
+          navigate('/')
         } else {
-          navigate("/login");
+          navigate('/login')
         }
       }
     } catch (error) {
-      navigate("/login");
-      console.log(error);
+      navigate('/login')
+      console.log(error)
     }
   }
 
   useEffect(() => {
-    Login();
-  }
-  , []);
-
-
+    Login()
+  }, [])
 
   return (
-    <AuthContext.Provider value={{ islogin,user,url, setUser , Login , auth_intra42}}>
+    <AuthContext.Provider
+      value={{ islogin, user, url, setUser, Login, auth_intra42 }}
+    >
       {children}
     </AuthContext.Provider>
-  );
+  )
 }
