7b0dc2a136442f2ad9a76bbc5925d391798fa8c7
Khalid Bouychou <khbouych@e3r4p5.1337.ma>
2024-10-29 15:36:04 +0100

    reset

--- ft_trancendance1337/backend/pongame/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('api/game-state/<str:room_name>/', views.game_state_view, name='game_state_view'),
-    path('', views.user_data, name="pingpong"),
-]
