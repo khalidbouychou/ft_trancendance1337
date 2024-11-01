09415cb62f742d2fc1753fe7a401e0ec18df60a6
Khalid Bouychou <khbouych@e3r10p12.1337.ma>
2024-11-01 01:52:36 +0100

    update verify

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -186,7 +186,7 @@ REST_FRAMEWORK = {
 AUTH_USER_MODEL = 'login.Player'
 # JWT settings
 SIMPLE_JWT = {
-   'ACCESS_TOKEN_LIFETIME': timedelta(minutes=180),
+   'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
    'REFRESH_TOKEN_LIFETIME': timedelta(days=90),
    'ROTATE_REFRESH_TOKENS': True,
    'BLACKLIST_AFTER_ROTATION': True
