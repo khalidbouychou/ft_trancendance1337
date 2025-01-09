b2ad2264ced088761e72032bb45e7b91c701afd8
0x00Jeff <jeff_septiceye@protonmail.com>
2025-01-09 17:41:52 +0100

    Feat: added web3 module

---
+++ web3/tools/1.deploy_contract.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+var HelloWorld = artifacts.require('HelloWorld');
+
+module.exports = function(deployer) {
+	deployer.deploy(HelloWorld).then(async () => {
+		let instance = await HelloWorld.deployed();
+		let contractAddress = instance.address;
+		fs.writeFileSync('./contractAddress.txt', contractAddress);
+	}).catch(error => {
+		console.error("Error deploying HelloWorld:", error);
+	});
+};
